384270aa35e8c237910e74d8c0e56951bd15f9fe
adaohauzoho <adaohauzoho@Adaohas-MacBook-Air.local>
2016-04-01 22:22:33 -0400

    removed var to see difference

--- js/gallery.js
+++ js/gallery.js
@@ -52,7 +52,7 @@ mRequest.onreadystatechange = function() {
 	if (mRequest.readyState == 4 && mRequest.status == 200) {
 	try {
 // Let’s try and see if we can parse JSON (see next slide)
-		var mJson = JSON.parse(mRequest.responseText);
+		 mJson = JSON.parse(mRequest.responseText);
 // LOOP THROUGH the mJSON array here and fill up the
 // mImages array with GalleryImage objects
 // Let’s print out the JSON; It will likely show as “obj”
